d37311589d878bb885439e1612928647bb54e5f9
shahzadulhaq765-source <shahzadulhaq765@gmail.com>
2026-08-06 23:22:11 +0500

    Add files via upload

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,16 +1,7 @@
-pluginManagement {
-    repositories {
-        google()
-        mavenCentral()
-        gradlePluginPortal()
-    }
-}
+pluginManagement { repositories { google(); mavenCentral(); gradlePluginPortal() } }
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    repositories {
-        google()
-        mavenCentral()
-    }
+    repositories { google(); mavenCentral() }
 }
-rootProject.name = "SalesLedger"
+rootProject.name = "SUHSalesPro"
 include(":app")
