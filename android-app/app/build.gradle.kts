a8b0ae57949a4f2f94421e06818f8bcb37c4de39
shahzadulhaq765-source <shahzadulhaq765@gmail.com>
2026-08-07 18:45:09 +0500

    Add files via upload

--- android-app/app/build.gradle.kts
+++ android-app/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.suh.salespro"
         minSdk = 24
         targetSdk = 35
-        versionCode = 3
-        versionName = "3.0"
+        versionCode = 4
+        versionName = "4.0"
     }
 
     compileOptions {
